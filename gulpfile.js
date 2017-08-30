0ff355ca6f528f6ffe12d60bc9a8e7a2f44aca4f
Kirill Volgin <volgin.kirill@gmail.com>
2017-08-30 22:57:11 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const elixir = require('laravel-elixir');
+require('laravel-elixir-postcss');
+
+Elixir.config.sourcemaps = true;
+Elixir.config.assetsPath = './resources/assets/';
+Elixir.config.publicPath = './public/assets/';
+
+var paths = {
+    srcFront      : Elixir.config.assetsPath + 'front/',
+    destFront     : Elixir.config.publicPath + 'front/',
+    srcDashboard  : Elixir.config.assetsPath + 'dashboard/',
+    destDashboard : Elixir.config.publicPath + 'dashboard/',
+    node          : './node_modules/'
+};
+
+elixir((mix) => {
+
+    // <START> Frontend Compilation
+    processors = [
+        require('precss'),
+        require('postcss-assets')({
+            baseUrl: 'public/',
+            loadPaths: ['assets/front/img/'],
+            relative: true,
+            cachebuster: true
+        }),
+        require('autoprefixer')({
+            browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+        }),
+        require('cssnano')({
+            convertValues: {
+                length: false
+            },
+            discardComments: {
+                removeAll: true
+            }
+        }),
+    ];
+
+
+    mix.postcss('app.css', {
+        srcDir  : paths.srcFront  + 'sass/',
+        output  : paths.destFront + 'css/',
+        plugins : processors
+    });
+
+    mix.scripts([
+        paths.node + 'jquery/dist/jquery.min.js',
+        paths.node + 'fastclick/lib/fastclick.js',
+        paths.node + 'slick-carousel/slick/slick.js',
+        paths.node + 'fancybox/dist/js/jquery.fancybox.js',
+        'app.js'
+    ],  paths.destFront  + 'js/app.min.js', paths.srcFront + 'js');
+    // <END> Frontend Compilation
+
+});
---
+++ resources/assets/front/js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    setTimeout(function(){
+        $('.notification').fadeOut()
+    }, 5000);
+
+    var hamburger = $('.hamburger-icon');
+    hamburger.click(function() {
+        hamburger.toggleClass('active');
+        hamburger.next().toggleClass('opened');
+        return false;
+    });
+
+    var anchor = window.location.hash;
+    if (anchor != "") {
+        $('body').animate({
+            scrollTop: $(anchor).offset().top}, 700);
+    }
+
+    $('a[href^="#"]').click(function(){
+        var anchor = $(this).attr('href');
+
+        $('body').animate({
+            scrollTop: $(anchor).offset().top}, 700);
+
+        setTimeout(function(){
+            window.location.hash = anchor;
+        }, 300);
+
+        if($('.hamburger-icon').hasClass('active')) {
+            setTimeout(function(){
+                $('.hamburger-icon').trigger('click');
+            }, 300);
+        }
+
+        return false;
+    });
+
+    $('.fancy').fancybox();
+
+    $('.slider').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        fade: false,
+        speed: 500,
+        autoplaySpeed: 7000
+    });
+
+    //Dropdown language
+    $(".dropdown .title").click(function () {
+        $(this).closest('.dropdown').toggleClass("opened");
+    });
+});
