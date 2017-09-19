cf67d65a83cb3c4f02247fb42ddf97bf8ef5d236
Andrey Boldyrev <runnez@yandex.ru>
2017-09-19 19:43:14 +0300

    update roadmap scroll effect

--- public/index.php
+++ public/index.php
@@ -330,61 +330,61 @@
     <article class="blue-roadmap" id="roadmap">
       <div class="wrapper">
         <h1 class="from-bottom">Our roadmap</h1>
-        <div class="timeline-wrap from-bottom">
+        <div class="timeline-wrap">
           <div class="timeline-bar">
             <div class="current"></div>
           </div>
 
           <div class="desc">
-            <div class="milestone active">
+            <div class="fade-in-left milestone active">
               <div class="title">
                 Launch of The Token Fund 
                 <div class="time">March</div>
               </div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-right milestone active">
               <div class="title">
                 More than 48,000 tokens sold to hundreds of investors
                 <div class="time">May</div>
               </div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-left milestone active">
               <div class="title">
                 The Fund's capitalisation reached $2 million, and the yield exceeded 300%
                 <div class="time">June</div>
               </div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-right milestone active">
               <div class="title">Tokenbox branding and naming, tech and operational team <div class="time">June</div></div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-left milestone active">
               <div class="title">Whitepaper & development plan <div class="time">July </div></div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-right milestone active">
               <div class="title">Advisory board & marketing plan  <div class="time">August</div>
             </div>
             </div>
-            <div class="milestone active">
+            <div class="fade-in-left milestone active">
               <div class="title"> Escrow, Pre-ICO marketing
                 <div class="time">September</div>
               </div>
             </div>
-            <div class="milestone">
+            <div class="fade-in-right milestone">
               <div class="title"> 
                 TGE (crowdsale) and legalisation process
                 <div class="time">October</div>
               </div>
             </div>
-            <div class="milestone">
+            <div class="fade-in-left milestone">
               <div class="title"> Listing on exchanges<div class="time">Q4 2017</div></div>
             </div>
-            <div class="milestone">
+            <div class="fade-in-right milestone">
               <div class="title">Completion of legalisation <div class="time">Q2 2018</div></div>
             </div>
-            <div class="milestone">
+            <div class="fade-in-left milestone">
               <div class="title"> Tokenbox platform, Alpha-version, due diligence & investments for the selected partners (funds & traders) <div class="time">Q3 2018</div></div>
             </div>
-            <div class="milestone">
+            <div class="fade-in-right milestone">
               <div class="title"> Beta-version of the platform, Tokenbox launch, new partners and investors on the platform<div class="time">Q4 2018</div></div>
             </div>
           </div>
@@ -727,7 +727,7 @@
       sr.reveal('.multi-crypto');
       sr.reveal('.media');
       sr.reveal('.footer');
-      sr.reveal('.milestone', 50);
+      // sr.reveal('.milestone');
       sr.reveal('.footer-social .link', 50)
       sr.reveal('.background-icon', {
         scale: 0.1,
