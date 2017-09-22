5217746edf22b6a543773141650ede471f00bf9e
Andrey Boldyrev <runnez@yandex.ru>
2017-09-22 22:50:55 +0300

    hide slack icon

--- public/index.php
+++ public/index.php
@@ -674,7 +674,7 @@
         <a href="https://www.facebook.com/Tokenbox/" class="link facebook" target="_blank"><span class="fa fa-facebook-square"></span></a>
         <a href="https://t.me/Tokenbox" class="link telegram" target="_blank"><span class="fa fa-telegram"></span></a>
         <a href="https://twitter.com/tokenbox" class="link twitter" target="_blank"><span class="fa fa-twitter-square"></span></a>
-        <a href="http://tokenbox.slack.com" class="link slack" target="_blank"><span class="fa fa-slack"></span></a>
+        <!--<a href="http://tokenbox.slack.com" class="link slack" target="_blank"><span class="fa fa-slack"></span></a>-->
         <a href="mailto:support@tokenbox.io" class="link email"><span class="fa fa-envelope-o"></span></a>
         <a href="https://www.linkedin.com/company-beta/11217933/" class="link linkedin" target="_blank"><span class="fa fa-linkedin-square"></span></a>
         <a href="https://www.reddit.com/r/tokenbox/" class="link reddit" target="_blank"><span class="fa fa-reddit-square"></span></a>
