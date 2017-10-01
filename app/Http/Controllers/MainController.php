8edcab479dde2fcfd33e39ef8df9e5568d66ae94
Aleksandr Volkov <avolkov05@gmail.com>
2017-10-02 00:43:32 +0700

    add client for intercom

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cryptovolk
+ * Date: 01.10.17
+ * Time: 22:53
+ */
+
+namespace App\Http\Controllers;
+
+
+class MainController
+{
+
+}
---
+++ app/Services/IntercomService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cryptovolk
+ * Date: 01.10.17
+ * Time: 23:57
+ */
+
+class IntercomService
+{
+
+}
