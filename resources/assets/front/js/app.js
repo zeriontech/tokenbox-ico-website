6b0913c23d50e0f9baf153dcce1b7913a318acb5
Andrey Boldyrev <runnez@yandex.ru>
2017-10-02 18:52:23 +0300

    fix modal overlay click, remove invalid css rules, fix modal scroll

--- resources/assets/front/js/app.js
+++ resources/assets/front/js/app.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-    var ww = ( $(window).width() < window.screen.width ) ? $(window).width() : window.screen.width; //get proper width
-    var mw = 400; // min width of site
-    var ratio =  ww / mw; //calculate ratio
-    if( ww < mw){ //smaller than minimum size
-      $('#Viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes, width=' + ww);
-    }else{ //regular size
-      $('#Viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=yes, width=' + ww);
-    }
-  }
-
-  // setTimeout(function(){
-  //   $('.notification').fadeOut()
-  // }, 5000);
-
   var hamburger = $('.hamburger-icon');
   hamburger.click(function() {
     hamburger.toggleClass('active');
