5b18b386689397068a411ae47e23779943d608b3
Andrey Boldyrev <runnez@yandex.ru>
2017-10-05 19:05:49 +0300

    wip

--- resources/views/shared/powered.blade.php
+++ resources/views/shared/powered.blade.php
@@ -5,11 +5,15 @@
 
     <div class="powered-by-logos">
       <a href="https://thetoken.io" target="_blank">
-        <img src="/assets/front/img/ico/logo-tkn.svg" alt="" width="142">
+        <img src="/assets/front/img/ico/logo-tkn.svg" alt="TokenFund" width="142">
       </a>
 
       <a href="https://zerion.io" target="_blank">
-        <img src="/assets/front/img/ico/logo-zerion.svg" alt="" width="256">
+        <img src="/assets/front/img/ico/logo-zerion.svg" alt="Zerion" width="256">
+      </a>
+
+      <a href="https://cindicator.com">
+        <img src="/assets/front/img/ico/logo-cindicator.svg" alt="Cindicator" width="121">
       </a>
     </div>
   </div>
