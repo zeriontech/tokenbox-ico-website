b84d8f7e8f850156720168d43c919ad9a2f6c74e
Andrey Boldyrev <runnez@yandex.ru>
2017-10-06 10:22:43 +0300

    split roadmap to devices and roadmap

---
+++ resources/views/shared/devices.blade.php
@@ -0,0 +1,13 @@
+<section class="section-devices">
+    <div class="wrapper">
+        <h1 class="from-bottom">{{__('Smoothest crossplatform experience')}}</h1>
+        <h3 class="from-bottom">{{__('Web-interface as well as native applications for main platforms:')}}
+            <strong>{{__('PC, Mac, iOS, Android, VR')}}.</strong></h3>
+        <div class="devices">
+            <div class="ipad from-bottom"></div>
+            <div class="macbook from-bottom"></div>
+            <div class="watch from-bottom"></div>
+            <div class="iphone from-bottom"></div>
+        </div>
+    </div>
+</section>
