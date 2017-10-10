7fd3533526c8ebe9474f5e645ed85a74077d0fc9
Deusdictum <Deusdictum@users.noreply.github.com>
2017-10-10 11:16:12 +0300

    Update roadmap.blade.php
    fix dates

--- resources/views/shared/roadmap.blade.php
+++ resources/views/shared/roadmap.blade.php
@@ -49,121 +49,121 @@
         <div class="fade-in-right milestone">
           <div class="title">
               {{__(' Listing on exchanges')}}
-              <div class="time">{{__('October 2017')}}</div>
+              <div class="time">{{__('November 2017')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__(' Registration and licensing')}}
-              <div class="time">{{__('November 2017')}}</div>
+              <div class="time">{{__('Q1 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Multi-currency wallet')}}
-              <div class="time">{{__('November 2017')}}</div>
+              <div class="time">{{__('Q1 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Trading terminal for fund managers')}}
-              <div class="time">{{__('November 2017')}}</div>
+              <div class="time">{{__('Q1 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('KYC module')}}
-              <div class="time">{{__('November 2017')}}</div>
+              <div class="time">{{__('Q1 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Payment processing for fiat currencies')}}
-              <div class="time">{{__('November 2017')}}</div>
+              <div class="time">{{__('Q1 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Alpha- & Beta- versions of Tokenbox platform')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Tokenbox platform web-interface')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Tokenization for funds')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Parsing system and back-end')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Built-in analytics tools')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Rating System')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('ICO marketplace')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Own licensed capability for funds and traders on the platform within the "umbrella fund" (sublicense)')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Investments for selected partners (funds & traders)')}}
-              <div class="time">{{__('Q1 2018')}}</div>
+              <div class="time">{{__('Q2 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Tokenbox platform launch')}}
-              <div class="time">{{__('Q2 2018')}}</div>
+              <div class="time">{{__('Q3 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('New partners and investors on the platform')}}
-              <div class="time">{{__('Q2 2018')}}</div>
+              <div class="time">{{__('Q3 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Internal risk management system')}}
-              <div class="time">{{__('Q3 2018')}}</div>
+              <div class="time">{{__('Q4 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-right milestone">
           <div class="title">
               {{__('Mobile and desktop applications')}}
-              <div class="time">{{__('Q3 2018')}}</div>
+              <div class="time">{{__('Q4 2018')}}</div>
             </div>
         </div>
         <div class="fade-in-left milestone">
           <div class="title">
               {{__('Customer analytics module')}}
-              <div class="time">{{__('Q3 2018')}}</div>
+              <div class="time">{{__('Q4 2018')}}</div>
             </div>
         </div>
       </div>
