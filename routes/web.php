404b7db1e20b88b7fdc5d7d1f7b12005dc5edaee
Aleksandr Volkov <avolkov05@gmail.com>
2017-10-16 19:54:53 +0700

    del unused route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,4 @@ Route::group([
     Route::post('/send', 'MainController@addUser')->name('add-user');
     Route::post('/sendfull', 'MainController@addFullUser')->name('add-full-user');
     Route::post('/sendqueue', 'MainController@addQueueUser')->name('add-queue-user');
-
-    Route::post('/intercom/callback', 'MainController@callback')->name('intercom-callback');
 });
