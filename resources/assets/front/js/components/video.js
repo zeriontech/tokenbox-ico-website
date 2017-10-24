91ba0c783ed6330ccb245f82ab364e7378b85fe7
Andrey Boldyrev <runnez@yandex.ru>
2017-10-24 11:23:38 +0300

    update video hover

--- resources/assets/front/js/components/video.js
+++ resources/assets/front/js/components/video.js
@@ -2,5 +2,5 @@
 var videoMarkup = '<iframe src="https://www.youtube.com/embed/6oXTkR8t8mw?showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>'
 
 $('.video_play').click(function() {
-  $(this).after(videoMarkup);
+  $('.video').html(videoMarkup);
 });
--- resources/views/shared/video.blade.php
+++ resources/views/shared/video.blade.php
@@ -2,8 +2,10 @@
   <div class="wrapper">
     <p class="video-title">Watch this simple one-minute video explainer for Tokenbox.</p>
 
-    <div class="video" style="background-image: url(http://bitcoinist.com/wp-content/uploads/2017/09/token-fund-cofounders2.jpg)">
-      <div class="video_play"></div>
+    <div class="video">
+      <div class="video_preview" style="background-image: url(http://bitcoinist.com/wp-content/uploads/2017/09/token-fund-cofounders2.jpg)">
+        <div class="video_play"></div>
+      </div>
     </div>
   </div>
 </section>
