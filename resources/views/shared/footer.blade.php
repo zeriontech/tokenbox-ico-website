9d820399900baea039b0d10c3af0caf63da461a7
Deusdictum <Deusdictum@users.noreply.github.com>
2017-10-24 18:09:47 +0300

    fix target blank

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -8,7 +8,7 @@
     <img class="logo" src="/assets/front/img/logo.svg">
     <a href="#terms" class="js-modal-link">{{__('Terms & Conditions')}}</a>
     <a href="#privacy" class="js-modal-link">{{__('User Privacy')}}</a>
-    <a href="/docs/Tokenbox-Certificate-of-Inc.pdf" target="blank">{{__('Legal Entity')}}</a>
+    <a href="/docs/Tokenbox-Certificate-of-Inc.pdf" target="_blank">{{__('Legal Entity')}}</a>
     <br><h2>&copy; 2017 Tokenbox LLC, Cayman Islands. Tokenbox.io</h2>
     </div>
   </div>
