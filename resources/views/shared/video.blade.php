0bc599372b83f4915223cc26e760a1cef9b46efe
Deusdictum <Deusdictum@users.noreply.github.com>
2017-10-27 21:03:34 +0300

    upd locales

--- resources/views/shared/video.blade.php
+++ resources/views/shared/video.blade.php
@@ -1,6 +1,6 @@
 <section class="section section-video">
   <div class="wrapper">
-    <p class="video-title">Watch this simple three-minute video explainer for Tokenbox.</p>
+    <p class="video-title">{{__('Watch this simple three-minute video explainer for Tokenbox')}}.</p>
 
     <div class="video">
       <div class="video_preview" style="background-image: url(/assets/front/img/video-thumb.jpg)">
