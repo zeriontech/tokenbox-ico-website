36f18317d3526d12ce6c22d7e28ffb0dc9e146bc
Andrey <runnez@yandex.ru>
2017-11-08 10:07:12 +0300

    update media and partners logos

--- resources/views/shared/media.blade.php
+++ resources/views/shared/media.blade.php
@@ -1,72 +1,26 @@
 <section class="media section">
     <div class="wrapper media-blocks">
         <h1 class="from-bottom">{{__('Media about Tokenbox.io')}}</h1>
-        <div class="row no-mobile">
-            <div class="three columns">
-                <a href="https://www.forbes.com/sites/mfonobongnsehe/2017/10/19/russian-bitcoin-tycoon-expands-into-africa/#30c8db6514db" target="_blank" class="img forbes"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://finance.yahoo.com/news/token-fund-founders-set-october-151900653.html" target="_blank" class="img yahoo"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://www.marketwatch.com/story/the-token-fund-welcomes-chris-skinner-on-board-2017-09-13" target="_blank" class="img marketwatch"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://cointelegraph.com/press-releases/new-cryptocurrency-asset-trading-platform-announced-by-founders-of-the-token-fund" target="_blank" class="img cointelegraph"></a>
-            </div>
-        </div>
-        <div class="row no-mobile">
-            <div class="three columns">
-                <a href="https://www.cryptocoinsnews.com/ethereum-used-ctfs-coin-traded-funds-benefits/" target="_blank" class="img cryptocoinsnews"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://bitcoinist.com/interview-with-token-fund-the-etf-alternative-for-cryptocurrency-investing/" target="_blank" class="img bitcoinist"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://www.coinspeaker.com/2017/09/28/the-token-fund-founders-set-october-launch-for-tokenbox-platform-for-crypto-assets-fund-creation/" target="_blank" class="img coinspeaker"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://www.investing.com/analysis/coin-traded-funds-new-kind-of-investment-funds-specializing-in-cryptocu-200204770" target="_blank" class="img investingcom"></a>
-            </div>
-        </div>
-        <div class="row no-mobile">
-            <div class="three columns">
-                <a href="https://actualidad.rt.com/programas/zoom/253094-criptodivisas-manzana-discordia-bancos-usuarios-gobiernos" target="_blank" class="img" style="background-image: url(/assets/front/img/media/rt.svg); background-size: 76px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://themerkle.com/now-there-is-an-all-purpose-solution-for-fund-managers-and-investors-meet-tokenbox/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/themerkle.png); background-size: 88px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://www.newsbtc.com/2017/11/01/tokenbox-sets-mission-build-legal-secure-competitive-fund-management-community-shield-investors-shady-operators/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/newsbtc.png); background-size: 128px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://www.intellinews.com/cryptocurrencies-more-than-a-token-effort-131334/?source=baltic-states" target="_blank" class="img" style="background-image: url(/assets/front/img/media/bne-intellinews.png); background-size: 180px auto"></a>
-            </div>
-        </div>
-        <div class="row no-mobile">
-            <div class="three columns">
-                <a href="https://www.wealthdaily.com/articles/how-investors-can-weather-the-bitcoin-storm/8949" target="_blank" class="img" style="background-image: url(/assets/front/img/media/wealthdaily.png); background-size: 180px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://russia-insider.com/en/making-it-easy-invest-cryptos-russian-entrepreneur-unveils-tokenbox/ri21382#disqus_thread" target="_blank" class="img" style="background-image: url(/assets/front/img/media/russia-insider.png); background-size: 112px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://www.europeanbusinessreview.eu/page.asp?pid=2178" target="_blank" class="img" style="background-image: url(/assets/front/img/media/ebr.png); background-size: 170px auto"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://www.eureporter.co/frontpage/2017/10/02/thetokenfund-develops-new-tool-to-capitalize-on-crypto-space/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/eureporter.png); background-size: 160px auto"></a>
-            </div>
-        </div>
-        <div class="row no-mobile">
-            <div class="three columns">
-                <a href="http://www.forbes.ru/tehnologii/350439-pole-chudes-v-strane-ico-servis-kotorogo-net" target="_blank" class="img forbes"></a>
-            </div>
-            <div class="three columns">
-                <a href="https://cointelegraph.com/explained/investing-in-cryptocurrencies-explained" target="_blank" class="img cointelegraph"></a>
-            </div>
-            <div class="three columns">
-                <a href="http://bitcoinist.com/savvy-ico-investor-rules-pick-right-project-participate/" target="_blank" class="img bitcoinist"></a>
-            </div>
+        <div class="no-mobile">
+            <a href="https://www.forbes.com/sites/mfonobongnsehe/2017/10/19/russian-bitcoin-tycoon-expands-into-africa/#30c8db6514db" target="_blank" class="img forbes"></a>
+            <a href="https://finance.yahoo.com/news/token-fund-founders-set-october-151900653.html" target="_blank" class="img yahoo"></a>
+            <a href="http://www.marketwatch.com/story/the-token-fund-welcomes-chris-skinner-on-board-2017-09-13" target="_blank" class="img marketwatch"></a>
+            <a href="https://cointelegraph.com/press-releases/new-cryptocurrency-asset-trading-platform-announced-by-founders-of-the-token-fund" target="_blank" class="img cointelegraph"></a>
+            <a href="https://www.cryptocoinsnews.com/ethereum-used-ctfs-coin-traded-funds-benefits/" target="_blank" class="img cryptocoinsnews"></a>
+            <a href="http://bitcoinist.com/interview-with-token-fund-the-etf-alternative-for-cryptocurrency-investing/" target="_blank" class="img bitcoinist"></a>
+            <a href="https://www.coinspeaker.com/2017/09/28/the-token-fund-founders-set-october-launch-for-tokenbox-platform-for-crypto-assets-fund-creation/" target="_blank" class="img coinspeaker"></a>
+            <a href="https://www.investing.com/analysis/coin-traded-funds-new-kind-of-investment-funds-specializing-in-cryptocu-200204770" target="_blank" class="img investingcom"></a>
+            <a href="https://actualidad.rt.com/programas/zoom/253094-criptodivisas-manzana-discordia-bancos-usuarios-gobiernos" target="_blank" class="img" style="background-image: url(/assets/front/img/media/rt.svg); background-size: 76px auto"></a>
+            <a href="https://themerkle.com/now-there-is-an-all-purpose-solution-for-fund-managers-and-investors-meet-tokenbox/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/themerkle.png); background-size: 88px auto"></a>
+            <a href="http://www.newsbtc.com/2017/11/01/tokenbox-sets-mission-build-legal-secure-competitive-fund-management-community-shield-investors-shady-operators/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/newsbtc.png); background-size: 128px auto"></a>
+            <a href="http://www.intellinews.com/cryptocurrencies-more-than-a-token-effort-131334/?source=baltic-states" target="_blank" class="img" style="background-image: url(/assets/front/img/media/bne-intellinews.png); background-size: 180px auto"></a>
+            <a href="https://www.wealthdaily.com/articles/how-investors-can-weather-the-bitcoin-storm/8949" target="_blank" class="img" style="background-image: url(/assets/front/img/media/wealthdaily.png); background-size: 180px auto"></a>
+            <a href="http://russia-insider.com/en/making-it-easy-invest-cryptos-russian-entrepreneur-unveils-tokenbox/ri21382#disqus_thread" target="_blank" class="img" style="background-image: url(/assets/front/img/media/russia-insider.png); background-size: 112px auto"></a>
+            <a href="http://www.europeanbusinessreview.eu/page.asp?pid=2178" target="_blank" class="img" style="background-image: url(/assets/front/img/media/ebr.png); background-size: 170px auto"></a>
+            <a href="https://www.eureporter.co/frontpage/2017/10/02/thetokenfund-develops-new-tool-to-capitalize-on-crypto-space/" target="_blank" class="img" style="background-image: url(/assets/front/img/media/eureporter.png); background-size: 160px auto"></a>
+            <a href="http://www.forbes.ru/tehnologii/350439-pole-chudes-v-strane-ico-servis-kotorogo-net" target="_blank" class="img forbes"></a>
+            <a href="https://cointelegraph.com/explained/investing-in-cryptocurrencies-explained" target="_blank" class="img cointelegraph"></a>
+            <a href="http://bitcoinist.com/savvy-ico-investor-rules-pick-right-project-participate/" target="_blank" class="img bitcoinist"></a>
         </div>
 
         <div class="mobile-only">
@@ -108,6 +62,23 @@
             </div>
         </div>
 
+        <h1>Ratings</h1>
+
+        <div class="media-rating">
+            <a href="http://icorating.com/project/581/Tokenbox" target="_blank" class="img img-rating">
+                <img src="/assets/front/img/media/ico-rating.png" width="192">
+                <div style="color: #389012; font-size: 16px;">
+                    <b style="font-size: 21px;">Stable</b><br>
+                    Hype score: VERY HIGH
+                </div>
+            </a>
+            <a href="https://icobench.com/ico/tokenbox/ratings#ratings" target="_blank" class="img img-rating">
+                <img src="/assets/front/img/media/ico-bench.png" width="167">
+                <div style="color: #a4c043; font-size: 51px; font-weight: 700;line-height: 1">
+                    4.4
+                </div>
+            </a>
+        </div>
 <!-- 
         <section class="medium" style="display: none;">
             <div class="medium-icon"></div>
