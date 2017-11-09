3684ed9d1b5583ac785a74f8567a05fc76d1118e
Andrey <runnez@yandex.ru>
2017-11-09 16:34:26 +0300

    update digest

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -9,7 +9,7 @@
   <link rel="icon" type="image/png" href="/favicon-144.png" sizes="144x144">
   <link rel="apple-touch-icon" href="/favicon-195.png?a" sizes="195x195">
 
-  <link rel="stylesheet" href="/assets/front/css/app.css?v=17">
+  <link rel="stylesheet" href="/assets/front/css/app.css?v=18">
 
   <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
 
@@ -70,7 +70,7 @@
   @include('shared/privateSaleForm')
   @include('shared/wireForm')
   
-  <script type="text/javascript" src="/assets/front/js/app.min.js?v=13"></script>
+  <script type="text/javascript" src="/assets/front/js/app.min.js?v=14"></script>
   <!-- Facebook Pixel Code -->
   <script>
     !function(f,b,e,v,n,t,s)
