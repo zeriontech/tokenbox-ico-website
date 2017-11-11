1991125e1b5d67a1ccf24a88aa22ac0218d90861
Andrey <runnez@yandex.ru>
2017-11-11 19:55:01 +0300

    update video slider reveal

--- resources/views/shared/videoSlider.blade.php
+++ resources/views/shared/videoSlider.blade.php
@@ -1,4 +1,4 @@
-<section class="section section-video fade-in-right">
+<section class="section section-video from-bottom">
   <div class="wrapper">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.3/css/swiper.min.css">
