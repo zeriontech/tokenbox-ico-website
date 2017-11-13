0067255a8a471d7429a5e1cbc093a6cc7866ccb0
Deusdictum <Deusdictum@users.noreply.github.com>
2017-11-13 22:18:28 +0300

    fix link color

--- resources/views/ico.blade.php
+++ resources/views/ico.blade.php
@@ -199,7 +199,7 @@
         <a href="#wire">{!!__('Request Wire Transfer?')!!}</a>
       </div>
 
-      <p style="font-size:2.6em;">{!!__('Got questions?')!!} <a href="{!!__('/docs/Tokenbox-TGE-Guide-En.pdf')!!}" target="_blank" style="font-weight:bold;">{!!__('See the detailed guide.')!!}</a></p><p>{!!__('If you decide to purchase more tokens, repeat steps 3-4 by signing-in to your <a href="https://tokebox.zerion.io/" target="_blank">dashboard</a>.')!!}</p>
+      <p style="font-size:2.6em;">{!!__('Got questions?')!!} <a href="{!!__('/docs/Tokenbox-TGE-Guide-En.pdf')!!}" target="_blank" style="font-weight:bold; color:#3434bb;">{!!__('See the detailed guide.')!!}</a></p><p>{!!__('If you decide to purchase more tokens, repeat steps 3-4 by signing-in to your <a href="https://tokebox.zerion.io/" target="_blank" style="color:#3434bb;">dashboard</a>.')!!}</p>
 @else
       <div class="button button-centred button-waiting" style="cursor: text">
         {{__('14 November')}}
