5cf34284974ef0574eaa650fe3ce0fde3a80ac62
Deusdictum <Deusdictum@users.noreply.github.com>
2017-11-14 21:23:45 +0300

    fix tooltip link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 			<div class="tooltip">
 			<b>{{__('TGE - Token Generation Event.')}}</b>
 			<br>
-			{{__('It is a synonym of the ICO. We use the TGE term because TBX tokens but not coins will be issued.')}}<br><a href="/">{{__('Learn more')}} &rarr;</a>
+			{{__('It is a synonym of the ICO. We use the TGE term because TBX tokens but not coins will be issued.')}}<br><a href="/tge">{{__('Learn more')}} &rarr;</a>
 			</div>
 		  </sup>
           {{__('TGE is on:')}} <span style="color:#ee3d49; font-weight: bold;">{!!__('15% bonus')!!}</span> — 3 {{__('days left!')}}
