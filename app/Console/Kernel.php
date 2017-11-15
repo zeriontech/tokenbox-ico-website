dc818311760b1cb7528c438651e04401aeac63fe
Aleksandr Volkov <avolkov05@gmail.com>
2017-11-15 19:30:37 +0700

    add send logger

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\MailchimpToIntercom;
+use App\Console\Commands\SendLog;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         MailchimpToIntercom::class,
+        SendLog::class,
     ];
 
     /**
@@ -26,6 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command(MailchimpToIntercom::class)->everyTenMinutes();
+         $schedule->command(SendLog::class)->hourly();
     }
 
     /**
