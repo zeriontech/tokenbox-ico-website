c1703c5127ddc17e5db1628553fa5b8cbbe0c3c9
Andrey Boldyrev <runnez@yandex.ru>
2017-11-17 14:38:10 +0900

    update digest

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -10,7 +10,7 @@
   <link rel="icon" type="image/png" href="/favicon-144.png" sizes="144x144">
   <link rel="apple-touch-icon" href="/favicon-195.png?a" sizes="195x195">
 
-  <link rel="stylesheet" href="/assets/front/css/app.css?v=27">
+  <link rel="stylesheet" href="/assets/front/css/app.css?v=28">
 
   <style>
     .sr-enabled .topblock .fade-in-item {
