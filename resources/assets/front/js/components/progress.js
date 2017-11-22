47360e0e670c3d0d41f88c78003a029fcc931b39
Andrey Boldyrev <runnez@yandex.ru>
2017-11-22 16:57:53 +0900

    fix progress tier label

--- resources/assets/front/js/components/progress.js
+++ resources/assets/front/js/components/progress.js
@@ -17,7 +17,7 @@ function updateProgress() {
     var total = +data.total_funds_raised + window.icoProgress.total;
     var target = total < 3e6 ? 3e6 : 10e6;
     var $label = $('.js-progress-target-label');
-    $label.text($label.data(total < 3e6 ? 'third-title' : 'second-title'));
+    $label.text($label.data(total > 3e6 ? 'third-title' : 'second-title'));
 
     $('.js-progress-target').text(target.toLocaleString('en'));
 
