d865a8400d3455861020765fbeaeaac688b3f206
Deusdictum <Deusdictum@users.noreply.github.com>
2017-11-23 20:17:54 +0300

    prepare Black Friday title

--- resources/views/shared/tgeTitle.blade.php
+++ resources/views/shared/tgeTitle.blade.php
@@ -13,6 +13,21 @@
   </span>-->
 </h1>
 
+<!-- Black Friday -->
+<!-- 
+<h1>
+  <sup data-tooltip>
+    <i class="fa fa-question-circle"></i>
+    <div class="tooltip">
+    <b>{{__('TGE - Token Generation Event.')}}</b>
+    <br>
+    {{__('It is a synonym of the ICO. We use the TGE term because TBX tokens but not coins will be issued.')}}<br><a href="#faq">{{__('Learn more')}} &rarr;</a>
+    </div>
+  </sup>
+  {{__('TGE is live')}}! <span style="font-weight:bold; text-transform:uppercase;">{{__('Black Friday')}} &mdash; <span style="color:#ee3d49;">5% {{__('off')}}!</span></span>
+</h1>
+ -->
+
 <p class="topblock_subtitle inline-countdown" style="opacity: 0;">
   {{__('Time Left')}}:
 
