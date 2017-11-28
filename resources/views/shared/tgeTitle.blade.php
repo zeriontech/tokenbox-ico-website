f7ac03f4155b328f34c190453859a4edb9f5c781
Deusdictum <Deusdictum@users.noreply.github.com>
2017-11-28 15:00:38 +0300

    fix tge close

--- resources/views/shared/tgeTitle.blade.php
+++ resources/views/shared/tgeTitle.blade.php
@@ -1,26 +1,3 @@
-<h1 style="font-weight:bold;">
-  <sup data-tooltip>
-    <i class="fa fa-question-circle"></i>
-    <div class="tooltip">
-    <b>{{__('TGE - Token Generation Event.')}}</b>
-    <br>
-    {{__('It is a synonym for ICO. The term TGE is used because we issue tokens — not coins.')}}<br><a href="#faq">{{__('Learn more')}} &rarr;</a>
-    </div>
-  </sup>
-  {{__('Token Generation Event')}} <span style="color:#ee3d49">{{__('is live')}}!</span>
-</h1>
-
-<p class="topblock_subtitle inline-countdown" style="opacity: 0;">
-  {{__('Time Left')}}:
-
-  <span data-key="days"></span>{{__('d')}}
-  <span data-key="hours"></span>{{__('h')}}
-  <span data-key="minutes"></span>{{__('m')}}
-  <span data-key="seconds"></span>{{__('s')}}
-</p>
-
-<!-- TGE is over -->
-<!-- 
 <h1>
   <sup data-tooltip>
     <i class="fa fa-question-circle"></i>
@@ -32,4 +9,3 @@
   </sup>
   {{__('Token Generation Event')}}<br /><span style="font-weight:bold; color:#ee3d49; text-transform:uppercase;">{{__('is over')}}</span>
 </h1>
- -->
