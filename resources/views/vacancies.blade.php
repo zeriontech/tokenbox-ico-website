e6a826ae2457c740fcc7f387508e8f9676e2f998
Andrey Boldyrev <runnez@yandex.ru>
2017-12-28 16:18:42 +0700

    vacancies i18n

--- resources/views/vacancies.blade.php
+++ resources/views/vacancies.blade.php
@@ -54,54 +54,10 @@
     @endcomponent
   </div>
 
-  <div class="vacancies">
-    <div class="wrapper">
-      <h1>{{__('Vacancies')}}</h1>
-
-      <div class="accordion">
-        <div class="accordion-item">
-            <input type="checkbox" id="item-1">
-
-            <label class="heading-2" for="item-1">
-                {{__("Product Design Manager")}}
-            </label>
-
-            <div class="accordion-item-content">
-                <p><b>What you'll be doing</b></p>
-                <p>
-                  {{__("Webflow has huge ambitions to fundamentally change the way websites and web applications are designed and built. We believe that web development should be accessible to anyone who loves to create, not just those who can understand the complexities of code. We're looking for someone who shares this vision for the future and who is enthusiastic about building creative tools for designers, entrepreneurs, marketers, and other non-technical professionals.")}}
-                </p>
-                <p>{{__("Webflow is looking for an experienced Product Design Manager to lead a team of designers. You'll help the team to translate powerful code-based paradigms into intuitive visual tooling. Candidates must have strong mentoring, communication skills. The role combines growing and managing a team with some design contribution. Candidates must have strong visual and interaction skills and the ability to help shape product strategy. You'll guide the team through some of the most critical parts of the product process: understanding user needs through research, creating interactive prototypes, user testing, designing high-fidelity interfaces, defining components with the engineering team, and extensively testing your product to make sure it meets all project goals.")}}</p>
-                
-                <ul>
-                  <li>One point</li>
-                  <li>Two point</li>
-                </ul>
-
-                <a href="" class="small-info-button">{{__('Respond to vacacancy')}}</a>
-            </div>
-        </div>
-
-        <div class="accordion-item">
-            <input type="checkbox" id="item-2">
-
-            <label class="heading-2" for="item-2">
-                {{__('Integration Engineer')}}
-            </label>
-
-            <div class="accordion-item-content">
-                <p>
-                  {{__("We are looking for ETL specialists interested in early stage opportunities building customer integrations for our telecom data platform and machine learning applications.")}}
-
-                </p>
-                
-                <p>
-                  As part of our small team, you will be responsible for creating the connections between customers' data systems and Adazza’s data platform. You will work with customers to understand the format and semantics of their data, build feeds to ingest this data into our platform, and create the transformations needed to enable our machine learning applications. You will collaborate with our development team to improve tooling and make customer integrations faster, easier, and more reliable.
-                </p>
-            </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  @if (LaravelLocalization::getCurrentLocale() == 'ru')
+    @include('shared/vacancies/ru')
+  @else
+    @include('shared/vacancies/en')
+  @endif
 @endsection
 
