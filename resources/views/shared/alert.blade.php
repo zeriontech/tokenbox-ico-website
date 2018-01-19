817e26825b3fb30e205cf341d8aaced97e6e301b
Deusdictum <deusdictum@gmail.com>
2018-01-19 21:16:06 +0300

    upd alert bar

--- resources/views/shared/alert.blade.php
+++ resources/views/shared/alert.blade.php
@@ -1,3 +1,29 @@
-<div class="w3-container" style="background: linear-gradient(90deg, #a4afbf, #818a97, #a4afbf);">
+<!--<div class="w3-container" style="background: linear-gradient(90deg, #a4afbf, #818a97, #a4afbf);">
 	<div style="color: white; padding: 13px 20px; text-align: center;">We are happy to announce that the TBX tokens are now listed on <a href="https://www.bancor.network/discover" target="_blank" style="color: white; font-weight: bold;">Bancor.network</a> and <a href="https://yobit.net/en/trade/TBX/USD" target="_blank" style="color: white; font-weight: bold;">YObit.net</a>!</div>
+</div>-->
+
+<style id="alert-bar-css" type="text/css">
+	.alert-bar {
+		color: #474c53;
+		font-family: Gilroy;
+		font-size: 15px;
+		display: block;
+		padding: 20px 15px;
+		text-align: center;
+		background: rgba(233, 236, 237, .9);
+	}
+	.alert-bar a { text-decoration: none; }
+	.alert-btn {
+		display: inline-block;
+		border-radius: 3px;
+		border: solid 3px #3434bb;
+		margin: 10px 10px 0px 10px;
+		font-weight: 800;
+		text-align: center;
+		color: #3434bb;
+		padding: 10px 10px;
+	}
+	.alert-btn:hover { background: #3434bb; color: #ffffff;	 }
+</style>
+<div class="alert-bar">TBX tokens are listed on <b>YObit</b> and <b>Bancor</b> <a href="#"><div class="alert-btn">Buy on YObit</div></a><a href="#"><div class="alert-btn">Buy on Bancor</div></a>
 </div>
